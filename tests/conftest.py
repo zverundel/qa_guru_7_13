cdbb18102d05539c124c9724d05592410042da3f
Daniil Zverev <svarhik348@gmail.com>
2023-10-11 23:35:34 +0300

    Изменения в conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,32 @@
-import pytest
+import os
 from selene import browser
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+from dotenv import load_dotenv
+
 from utils import attach
+
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function')
-def setup_browser():
-    browser_version = "100.0"
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -18,20 +37,22 @@ def setup_browser():
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-
-    browser.config.driver = driver
+    browser = Browser(Config(driver))
 
     yield browser
 
-    attach.add_screenshot(browser)
     attach.add_html(browser)
+    attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
-
     browser.quit()
 
 
